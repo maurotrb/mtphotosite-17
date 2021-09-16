96f103783254d18a8b885bc3b84b42a78fc95680
Mauro Taraborelli <mauro@maurotaraborelli.com>
2021-09-16 21:47:10 +0200

    Update tags in the script to clean images metadata

--- src/CleanJPGMetadata.sh
+++ src/CleanJPGMetadata.sh
@@ -7,18 +7,31 @@ exiftool -all= \
          -ICC_Profile \
          -EXIF:Artist \
          -EXIF:Copyright \
+         -EXIF:UserComment \
+         -EXIF:ExifImageWidth \
+         -EXIF:ExifImageHeight \
+         -EXIF:OwnerName \
          -EXIF:GPSVersionID \
          -EXIF:GPSLatitudeRef \
          -EXIF:GPSLatitude \
          -EXIF:GPSLongitudeRef \
          -EXIF:GPSLongitude \
-         -EXIF:ImageDescription \
+         -XMP:Caption \
+         -XMP:GPSLatitudeRef \
+         -XMP:GPSLongitudeRef \
+         -XMP:GPSMapDatum \
+         -XMP:GPSVersionID \
+         -XMP:GPSLongitude \
+         -XMP:GPSLatitude \
+         -XMP:GPSAltitudeRef \
+         -XMP:GPSAltitude \
+         -XMP:Artist \
+         -XMP:CreatorWorkEmail \
+         -XMP:CreatorWorkURL \
+         -XMP:Copyright \
+         -XMP:ImageDescription \
          -XMP:Creator \
          -XMP:Title \
          -XMP:Description \
          -XMP:Rights \
-         -XMP:UsageTerms \
-         -XMP:CreatorWorkEmail \
-         -XMP:CreatorWorkURL \
-         -IPTC:CopyrightNotice \
          $*
